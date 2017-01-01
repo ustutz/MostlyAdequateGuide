06ce52eb7528d16e34bf03d9fe3e1978f7f44afa
Urs Stutz <gitter@ls3d.de>
2017-01-01 19:41:56 +0100

    currying exercises

--- curry/src/Main.hx
+++ curry/src/Main.hx
@@ -48,6 +48,70 @@ class Main {
 	
 	static function curryingExcercises():Void {
 		
+		// Exercise 1
+		//==============
+		// Refactor to remove all arguments by partially applying the function.
+		
+		var words = function( str:String ):Array<String> return str.split( ' ' );
+		
+		trace( "words( 'alpha beta gamma delta' ): " + words( 'alpha beta gamma delta' ));
+		
+		
+		
+		// Exercise 1a
+		//==============
+		// Use map to make a new words fn that works on an array of strings.
+
+		var sentences = Lambda.map.bind( _, words );
+
+		
+		
+		// Exercise 2
+		//==============
+		// Refactor to remove all arguments by partially applying the functions.
+		
+		var filterQs = Lambda.filter.bind( _,  ~/q/i.match.bind( _ ));
+		
+		trace( "filterQs( ['alpha', 'beta', 'gamma', 'delta'] ): " + filterQs( ['alpha', 'beta', 'gamma', 'delta'] ));
+		
+		
+		
+		// Exercise 3
+		//==============
+		// Use the helper function _keepHighest to refactor max to not reference any
+		// arguments.
+		
+		var keepHighest = function( x:Float, y:Float ):Float {
+			return x >= y ? x : y;
+		}
+		
+		var max = Lambda.fold.bind( _, keepHighest, Math.NEGATIVE_INFINITY );
+		
+		trace( max( [1, 4, 3, 2, 599, 244] ));
+		
+		
+		
+		// Bonus 1:
+		// ============
+		// wrap array's slice to be functional and curried.
+		// //[1,2,3].slice(0, 2)
+		
+		var slice = function( start:Int, end:Int, xs:Array<Int> ):Array<Int> return xs.slice( start, end );
+		
+		trace( 'slice( 1, 3, [0, 1, 2, 3, 4, 5, 6] ): ' + slice( 1, 3, [0, 1, 2, 3, 4, 5, 6] ));
+
+		
+		
+		// Bonus 2:
+		// ============
+		// Use slice to define a function "take" that takes n elements from the beginning of the string. Make it curried.
+		// // Result for "Something" with n=4 should be "Some"
+
+		var stringSlice = function( start:Int, end:Int, xs:String ):String return xs.substr( start, end );
+		var take = stringSlice.bind( 0, _, _ );
+		
+		trace( "take( 4, 'Something' ): " + take( 4, 'Something' ));
 	}
 	
+		
 }
